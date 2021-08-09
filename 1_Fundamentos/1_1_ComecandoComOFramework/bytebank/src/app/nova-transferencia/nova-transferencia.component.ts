542b3ce708853c7a58e955ae6b45c46dac85b182
Jefferson S <jefferson.santos@radixeng.com.br>
2021-08-09 08:01:26 -0300

    Passando valores para as variáveis.

--- 1_Fundamentos/1_1_ComecandoComOFramework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ 1_Fundamentos/1_1_ComecandoComOFramework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,22 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
 })
 
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+
+  valor: number | undefined;
+  destino: number | undefined;
+
+
+  transferir() {
+    console.log("Valores ---->");
+    console.log("Valor :" + this.valor);
+    console.log("Valor :" + this.destino);
+
+  }
 
 }
