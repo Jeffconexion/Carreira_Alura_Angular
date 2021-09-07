93aae56188a22a7b61f9907de9eb33c2416516aa
Jefferson S <jefferson.santos@radixeng.com.br>
2021-09-07 11:22:03 -0300

    Trabalhando com modulos na aplicação angular.

--- 0_Curso_Loiane/1_Introducao/primeiro-projeto/src/app/app.module.ts
+++ 0_Curso_Loiane/1_Introducao/primeiro-projeto/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponentComponent } from './meu-primeiro-component/meu-primeiro-component.component';
 import { MeuSegundoComponentComponent } from './meu-segundo-component/meu-segundo-component.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       MeuPrimeiroComponentComponent,
       MeuSegundoComponentComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
