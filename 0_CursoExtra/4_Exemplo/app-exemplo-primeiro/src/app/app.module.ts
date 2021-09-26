4b0928f78080da4d04d8c4fcd483f95fcb947c0d
Jefferson S <jefferson.santos@radixeng.com.br>
2021-09-25 21:26:24 -0300

    Trabalhando com serviços.

---
+++ 0_CursoExtra/4_Exemplo/app-exemplo-primeiro/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { CursosService } from './cursos/cursos.service';
+import { CursosModule } from './cursos/cursos.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CursosModule
+  ],
+  providers: [CursosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
